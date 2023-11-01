dc3d990e5babc29c42c6bb5d037d5d66e0584e7e
eliudnyamai <eliudmitau@gmail.com>
2023-11-01 22:39:50 +0300

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-    @vite('resources/css/app.css')
+    <title>Convert Text2SQL</title>
+    @vite('resources/css/app.css')s
     <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
     <script>
         document.addEventListener("DOMContentLoaded", function (){
